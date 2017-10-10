1d8e5e7a1513d28d898c5beaf228db04f9ffc119
Arif16D <32444282+Arif16D@users.noreply.github.com>
2017-10-10 22:03:26 +0700

    Add files via upload

---
+++ prosesmenjadisarjana.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+cout << "****************************************" <<endl;
+cout << " Algoritma untuk menjadi sarjana " <<endl; 
+cout << "***************************************" <<endl;
+
+cout << "1.masuk kelas 1 SD" <<endl;
+cout << "2.naik kelas 2 SD" <<endl;
+cout << "3.naik kelas 3 SD" <<endl;
+cout << "4.naik kelas 4 SD" <<endl;
+cout << "5.naik kelas 5 SD" <<endl;
+cout << "6.naik kelas 6 SD" <<endl;
+cout << "7.lulus SD" <<endl;
+cout << "8.masuk kelas 1 SMP" <<endl;
+cout << "9.naik kelas 2 SMP" <<endl;
+cout << "10.naik kelas 3 SMP" <<endl;
+cout << "11.lulus SMP" <<endl;
+cout << "12.masuk kelas 1 SMA" <<endl;
+cout << "13.naik kelas 2 SMA" <<endl;
+cout << "14.naik kelas 3 SMA" <<endl;
+cout << "15.lulus SMA" << endl;
+cout << "16.masuk kuliah smester 1" << endl;
+cout << "17.naik smester 2" << endl;
+cout << "18.naik smester 3" << endl;
+cout << "19.naik smester 4" << endl; 
+cout << "20.naik smester 5" << endl;
+cout << "21.naik smester 6" << endl;
+cout << "22.naik smester 7" << endl;
+cout << "23.naik smester 8" << endl;
+cout << "24.buat modul" << endl;
+cout << "25.buat skripsi" << endl;
+cout << "26.lulus skripsi lanjut sidang" << endl;
+cout << "27.lulus sidang lanjut wisuda" << endl;
+cout << "28.sarjana" << endl;
+
+return 0;
+}
