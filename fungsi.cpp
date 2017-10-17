ec8d34d015d1307b00d960ef28cc3a76e1626814
Arif16D <32444282+Arif16D@users.noreply.github.com>
2017-10-17 22:31:31 +0700

    Add files via upload

---
+++ fungsi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int a,b;
+cout<<"maka nilai a";
+cin>> a;
+cout<<"maka nilai b";
+cin>> b;
+if (a>b) {
+cout<<a;
+}
+else if (a<b) {
+cout<<b;
+}
+return 0; 
+}
